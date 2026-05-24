b054e80c148304ab526c04c9a2fcc933daf41b9c
kocho <bhuvanvashishtha656@gmail.com>
2026-05-24 11:40:00 +0000

    TC -> O(n) , SC -> O(1)

---
+++ Strings/Valid_Palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.trim().replaceAll("[^A-Za-z0-9]","");
+        s = s.toLowerCase();
+        if(s.length() == 0) return true;
+        int left = 0;
+        int right = s.length()-1;
+        while(left<right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
