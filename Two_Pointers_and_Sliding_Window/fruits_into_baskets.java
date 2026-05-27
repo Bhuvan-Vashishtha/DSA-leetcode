96ec2841e1ad7ff731175e54be42690afdd590e1
kocho <bhuvanvashishtha656@gmail.com>
2026-05-27 13:13:51 +0000

    TC->O(n), SC-> O(1)

--- Two_Pointers_and_Sliding_Window/fruits_into_baskets.java
+++ Two_Pointers_and_Sliding_Window/fruits_into_baskets.java
@@ -2,9 +2,8 @@ class Solution {
     public int totalFruit(int[] fruits) {
         int left = 0;
         int right = 0;
-        int maxLen = 0;
-        int len = 0;
         int n = fruits.length;
+        int maxLen = 0;
         HashMap<Integer,Integer> map = new HashMap<>();
         while(right<n){
             map.put(fruits[right], map.getOrDefault(fruits[right],0)+1);
@@ -12,9 +11,9 @@ class Solution {
                 while(map.size()>2){
                     map.put(fruits[left], map.getOrDefault(fruits[left],0)-1);
                     if(map.get(fruits[left]) == 0){
-                    map.remove(fruits[left]);
-                }
-                left++;
+                        map.remove(fruits[left]);
+                    }
+                    left++;
                 }
             }
             if(map.size()<=2){
