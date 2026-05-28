c4ce783ff53b61bde9880d8f5bcaaed0946921f4
kocho <bhuvanvashishtha656@gmail.com>
2026-05-28 04:48:32 +0000

    TC->O(n), SC->O(1)

---
+++ Two_Pointers_and_Sliding_Window/subarrays_with_k_different_integers.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return helper(nums, k)-helper(nums,k-1);
+    }
+    static int helper(int[] nums, int k){
+        int left = 0;
+        int right = 0;
+        int n = nums.length;
+        int count = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        while(right<n){
+            map.put(nums[right], map.getOrDefault(nums[right],0)+1);
+            if(map.size()>k){
+                while(map.size()>k){
+                    map.put(nums[left], map.getOrDefault(nums[left],0)-1);
+                    if(map.get(nums[left]) == 0){
+                        map.remove(nums[left]);
+                    }
+                    left++;
+                }
+            }
+            count += right-left+1;
+            right++;
+        }
+        return count;
+    }
+}
